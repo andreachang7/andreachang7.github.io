62ccfaa2b5350fdf2a7da77935527a90ea8854c6
andreachang7 <chana206@newschool.edu>
2022-03-01 10:21:02 -0500

    update

--- 1-shadows-proj/script.js
+++ 1-shadows-proj/script.js
@@ -1,3 +1,7 @@
-function changeColor(color) {
-    document.body.style.background = color;
- }
+// function changeColor(color) {
+//     document.body.style.background = color;
+//  }
+
+function dayMode() {
+    document.getElementById("button-1").style.color = "red";
+}
