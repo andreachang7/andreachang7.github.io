bc3464d45e6ad10ec116c27c93c8ab318ef7fb1b
andreachang7 <chana206@newschool.edu>
2022-03-24 20:48:56 -0400

    update

--- 7-button/button.js
+++ 7-button/button.js
@@ -1,11 +1,19 @@
-var scrollTopBtn = document.getElementById("scrollTopBtn");
-var rootElement = document.documentElement;
-
-function scrollTop() {
-  // Scroll to top logic
-  rootElement.scrollTo({
-    top: 0,
-    behavior: "smooth"
-  });
+function changeTitle() {
+    document.getElementById("title").style.color = "#205FEA";
 }
-scrollTopBtn.addEventListener("click", scrollTop);
+
+function changeTitleTwo() {
+    document.getElementById("title").style.color = "green";
+}
+
+// var scrollTopBtn = document.getElementById("scrollTopBtn");
+// var rootElement = document.documentElement;
+
+// function scrollTop() {
+//   // Scroll to top logic
+//   rootElement.scrollTo({
+//     top: 0,
+//     behavior: "smooth"
+//   });
+// }
+// scrollTopBtn.addEventListener("click", scrollTop);
