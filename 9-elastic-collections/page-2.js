f7efbea8922d3f322486fccfbd5d805e397bc5a6
andreachang7 <chana206@newschool.edu>
2022-04-22 10:45:03 -0400

    update

--- 9-elastic-collections/page-2.js
+++ 9-elastic-collections/page-2.js
@@ -1,9 +1,6 @@
-// console.log("Hello, Airtable");
-
 let wrapper = document.querySelector(".wrapper");
 
 let Airtable = require("airtable");
-// console.log(Airtable);
 
 let base = new Airtable({ apiKey: "key936TACOIOQmwHd" }).base(
   "appKVn1znX1pUNSst"
@@ -39,91 +36,127 @@ function consoleLogMusicians() {
   });
 }
 
-// loop through the books, create an h2 for each one, and add it to the page
 function showMusicians() {
   console.log("showMusicians()");
   musicians.forEach((musician) => {
 
-      // let musicianName = document.createElement("div");
-      // musicianName.classList.add("item");
-      // musicianName.innerText = musician.fields.name;
-      // wrapper.append(musicianName);
+  var musicContainer = document.createElement("div");
+  musicContainer.classList.add("music-container");
+  document.querySelector(".wrapper").append(musicContainer);
 
-      let imageHolder = document.createElement("img");
+      var imageHolder = document.createElement("img");
       imageHolder.classList.add("images");
       imageHolder.src = musician.fields.photographs[0].url;
-      // musicianName.appendChild(imageHolder);
-      wrapper.append(imageHolder);
+      musicContainer.append(imageHolder);
 
+      var musicianName = document.createElement("h1");
+      musicianName.classList.add("musician-name");
+      musicianName.innerText = musician.fields.name;
+      musicContainer.append(musicianName);
 
+      var musicianGenre = document.createElement("h1");
+      musicianGenre.classList.add("musician-genre");
+      musicianGenre.innerText = musician.fields.genre;
+      musicContainer.append(musicianGenre);
 
+      var musicianStreet = document.createElement("p");
+      musicianStreet.classList.add("musician-street");
+      musicianStreet.innerText = musician.fields.streetave;
+      musicContainer.append(musicianStreet);
 
-    //   var nameofMusician = document.createElement("h1");
-    //   nameofMusician.innerText = song.fields.borough;
-    //   document.body.append(nameofMusician);
+      var musicianNeighborhood = document.createElement("p");
+      musicianNeighborhood = classList.add("musician-neighborhood");
+      musicianNeighborhood.innerText = musician.fields.neighborhood;
+      musicContainer.append(musicianNeighborhood);
+  
 
-    // let musiciansTextHolder = document.createElement("div");
-    // musiciansTextHolder.classList.add("entry");
-    // musiciansTextHolder.innerText = musician.fields.text;
-    //  wrapper.appendChild(musiciansTextHolder);
+// filter buttons //
+      musicContainer.addEventListener("click", function(){
+          musicianName.classList.toggle("active");
+      })
 
-    //  let imageHolder = document.createElement("image");
-    //  imageHolder.src = tiktok.fields.video[0].url;
-    //  imageHolder.classList.add("musicianImage");
-    //  imageHolder.muted = true;
-    //  imageHolder.autoplay = true;
-    //  imageHolder.loop = true;
-    //  musiciansTextHolder.appendChild(imageHolder);
+      var musicianLocation = musician.fields.locations;
+      musicianLocation.forEach(function(location){
+          musicContainer.classList.add(location)
+      })
+
+      var filterBronx = document.querySelector(".bronx");
+      filterBronx.addEventListener("click", function(){
+
+        if (musicContainer.classList.contains("bronx")){
+          musicContainer.style.background = "#AC2B2F";
+          musicContainer.style.border = "none";
+          musicContainer.style.display = "block";
+          filterBronx.classList.add("active");
+        } else {
+          musicContainer.style.background = "white";
+          musicContainer.style.display = "none";
+        }
+      })
+
+      var filterBrooklyn = document.querySelector(".brooklyn");
+      filterBrooklyn.addEventListener("click", function(){
+
+        if (musicContainer.classList.contains("brooklyn")) {
+          musicContainer.style.background = "#569D3B";
+          musicContainer.style.border = "none";
+          musicContainer.style.display = "block";
+          filterBrooklyn.classList.add("active");
+        } else {
+          musicContainer.style.background = "white";
+          musicContainer.style.display = "none";
+        }
+      })
+
+      var filterQueens = document.querySelector(".queens");
+      filterQueens.addEventListener("click", function(){
+
+        if (musicContainer.classList.contains("queens")) {
+          musicContainer.style.background = "#96C4EB";
+          musicContainer.style.border = "none";
+          musicContainer.style.display = "block";
+          filterQueens.classList.add("active");
+        } else {
+          musicContainer.style.background = "white";
+          musicContainer.style.display = "none";
+        }
+      })
+
+      var filterManhattan = document.querySelector(".manhattan");
+      filterManhattan.addEventListener("click", function(){
+
+        if (musicContainer.classList.contains("manhattan")) {
+          musicContainer.style.background = "orange";
+          musicContainer.style.border = "none";
+          musicContainer.style.display = "block";
+          filterManhattan.classList.add("active");
+        } else {
+          musicContainer.style.background = "white";
+          musicContainer.style.display = "none";
+        }
+      })
+
+      var filterStaten = document.querySelector(".staten");
+      filterStaten.addEventListener("click", function(){
+
+        if (musicContainer.classList.contains("staten")) {
+          musicContainer.style.background = "#C89FF3";
+          musicContainer.style.border = "none";
+          musicContainer.style.display = "block";
+          filterStaten.classList.add("active");
+        } else {
+          musicContainer.style.background = "white";
+          musicContainer.style.display = "none";
+        }
+      })
+
+      var filterReset = document.querySelector(".js-reset");
+      filterReset.addEventListener("click", function(){
+        if (musicContainer.classList.contains("bronx", "brooklyn", "manhattan", "queens", "staten")) {
+          musicContainer.style.background = "white";
+          musicContainer.style.display = "block";
+        }
+      })
+
   });
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var div = document.querySelector("div");
-// var ul = document.querySelector("ul");
-// var ol = document.querySelector("ol");
-
-// async function getPeople() {
-//   let response = await fetch(
-//     "https://api.airtable.com/v0/appKVn1znX1pUNSst/Table%201?api_key=keyNGA0AZ4vxvlL4x");
-//   let data = response.json();
-//   return data;
-// }
-
-// getPeople().then((data) => {
-//     console.log(data.records)
-//     makeList(data.records) 
-// });
-
-// function makeList(records) {
-//   for (let i = 0; i < records.length; i++) {
-//     let name = records[i].fields.name;
-//     let listItem = document.createElement("div");
-//     listItem.appendChild(document.createTextNode(name));
-//     div.appendChild(listItem);
-//   }
-//   for (let i = 0; i < records.length; i++) {
-//     let borough = records[i].fields.borough;
-//     let listItem = document.createElement("ol");
-//     listItem.appendChild(document.createTextNode(borough));
-//     div.appendChild(listItem);
-//   }
-//   for (let i = 0; i < records.length; i++) {
-//     let neighborhood = records[i].fields.neighborhood;
-//     let listItem = document.createElement("ol");
-//     listItem.appendChild(document.createTextNode(neighborhood));
-//     div.appendChild(listItem);
-//   }
-// }
-
