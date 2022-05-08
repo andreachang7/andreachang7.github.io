37210e2aa817d6e98b7da779e4a22c66998a0ce1
andreachang7 <chana206@newschool.edu>
2022-05-08 10:56:15 -0400

    update

--- 9-elastic-collections/page-3.js
+++ 9-elastic-collections/page-3.js
@@ -1,42 +1,9 @@
-dragElement(document.getElementById("mydiv"));
-
-function dragElement(elmnt) {
-  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  if (document.getElementById(elmnt.id + "header")) {
-    /* if present, the header is where you move the DIV from:*/
-    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-  } else {
-    /* otherwise, move the DIV from anywhere inside the DIV:*/
-    elmnt.onmousedown = dragMouseDown;
-  }
-
-  function dragMouseDown(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // get the mouse cursor position at startup:
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    document.onmouseup = closeDragElement;
-    // call a function whenever the cursor moves:
-    document.onmousemove = elementDrag;
-  }
-
-  function elementDrag(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // calculate the new cursor position:
-    pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    // set the element's new position:
-    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-  }
-
-  function closeDragElement() {
-    /* stop moving when mouse button is released:*/
-    document.onmouseup = null;
-    document.onmousemove = null;
-  }
+function randomColor() {
+  var color = '#';
+  var colorCode = ['88FDFC','3BF2BC','FB80B4','EF5E13','C4DF20', 'E13127', 'FFE6A3', '#1D3FDA', 'red']; 
+  var className = document.getElementsByClassName("change"); 
+  var i;
+  color += colorCode[Math.floor(Math.random() * colorCode.length)];
+for (var i = 0; i < className.length; i ++) {
+    className[i].style.backgroundColor = color; }
 }
