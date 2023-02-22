32a385a0bb68ae13528ed0d42ceea0c0fdca8c86
andreachang7 <chana206@newschool.edu>
2023-02-22 12:35:58 -0500

    update

---
+++ a-generative-web/1-project/draft-5.js
@@ -0,0 +1,37 @@
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    
+    angleMode(DEGREES)
+    rectMode(CENTER)
+  }
+  
+  function draw() {
+    background(0);
+    noFill();
+    stroke(255)
+    
+    translate(width / 2, height / 2)
+    
+    for (var i = 0; i < 200; i++) {
+      push()
+      
+      rotate(sin(frameCount + i) * 200)
+      
+      var r = map(sin(frameCount), -1, 1, 100, 255)
+      var g = map(cos(frameCount / 2), -1, 1, 10, 255)
+      var b = map(sin(frameCount / 4), -1, 1, 50, 255)
+      
+    //   stroke(random(5, 255), random(5, 255), random(5, 255))
+      stroke(r,g,b)
+
+    //   var a = (0, 0, 100 - i * 3, 100)
+    //   var c = (0, 0, 500 - i * 3, 50)
+    //   var d = (0, 0, 50 - i * 3, 500)
+      
+    //   rect(a,c,d)
+      
+      rect(0, 0, 100 - i * 3, 100 * 50)
+      
+      pop()
+    }
+  }
